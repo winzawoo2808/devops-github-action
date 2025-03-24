916a3b0bcf6b98420542685a707d465e8398c392
winzawoo <winzawster@gmail.com>
2025-03-24 12:18:56 +0800

    Github action Example

---
+++ src/main/java/com/example/devops_github_action/DevopsGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.devops_github_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsGithubActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devops_github_action/DevopsGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops_github_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsGithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
