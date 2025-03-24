b2ba28ca0a6c235b799e435eb8c46578878f76ea
winzawoo <winzawster@gmail.com>
2025-03-24 16:43:53 +0800

    upated hello API

--- src/main/java/com/example/devops_github_action/DevopsGithubActionApplication.java
+++ src/main/java/com/example/devops_github_action/DevopsGithubActionApplication.java
@@ -2,12 +2,19 @@ package com.example.devops_github_action;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DevopsGithubActionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsGithubActionApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	public String hello(){
+		return "Welcome to Github Actions";
+	}
 }
